290c9b5dead594c9ef31fa55cff033776a54ceb6
unknown <ygorsallesaniceto@gmail.com>
2020-12-16 10:54:51 -0300

    Finalizando subcategories

--- appFinal/versao-inicial/backend/api/article.js
+++ appFinal/versao-inicial/backend/api/article.js
@@ -76,7 +76,7 @@ module.exports = app => {
     const getByCategory = async (req, res) => {
         const categoryId = req.params.id
         const page = req.query.page || 1
-        const categories = await app.db.raw(queries.categoryWithChildren, req.params.id)
+        const categories = await app.db.raw(queries.categoryWithChildren, categoryId)
         const ids = categories.rows.map(c => c.id)
 
         app.db({a: 'articles', u: 'users'})
